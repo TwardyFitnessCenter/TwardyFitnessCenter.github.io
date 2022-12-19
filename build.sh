e266b06ca24ac4ed6b8402b14206efc70ec8c140
Hubert Furmanek <furmanek.hubert@gmail.com>
2022-12-19 23:20:11 +0000

    Apply comments from PR

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ project_name=placeholder
 preview_project_name=main
 echo Hello, this script will only copy folders in the correct format for our hosted website
 echo Project used on / - \"$project_name\"
-echo Project used on /preview - \"$subproject_name\"
+echo Project used on /preview - \"$preview_project_name\"
 
 build_project() {
     echo Build project \"$project_name\"
@@ -13,9 +13,9 @@ build_project() {
 }
 
 build_preview_project() {
-    echo Build preview project \"$subproject_name\"
-    npm --prefix src/$project_name/ install src/$subproject_name/
-    npm --prefix src/$project_name/ run build
+    echo Build preview project \"$preview_project_name\"
+    npm --prefix src/$preview_project_name/ install src/$preview_project_name/
+    npm --prefix src/$preview_project_name/ run build
 }
 
 copy_project() {
@@ -30,16 +30,16 @@ copy_project() {
 }
 
 copy_preview_project() {
-    echo Copy preview project files from \"$subproject_name\"
+    echo Copy preview project files from \"$preview_project_name\"
     mkdir docs/preview
     cp -r src/assets docs/preview
     cp -r src/lib docs/preview
     mkdir docs/preview/js
-    cp -r src/${subproject_name}/js/* docs/preview/js
+    cp -r src/${preview_project_name}/js/* docs/preview/js
     mkdir docs/preview/styles
     mkdir docs/preview/styles/css
-    cp -r src/${subproject_name}/styles/css/* docs/preview/styles/css
-    cp -r src/${subproject_name}/index.html docs/preview
+    cp -r src/${preview_project_name}/styles/css/* docs/preview/styles/css
+    cp -r src/${preview_project_name}/index.html docs/preview
 }
 
 copy_404() {
